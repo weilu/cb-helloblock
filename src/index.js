ef0f1e616fefcdcfc1dff71637753cbf995617c0
Daniel Cousens <github@dcousens.com>
2014-08-18 08:25:14 +1000

    index: map common network names to Helloblock urls

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 var Addresses = require('./addresses')
 var Transactions = require('./transactions')
 
+var NETWORKS = {
+  bitcoin: "mainnet",
+  testnet: "testnet"
+}
+
 function Helloblock(network) {
-  var BASE_URL = 'https://' + network + '.helloblock.io/v1/'
+  network = network || 'bitcoin'
+  var BASE_URL = 'https://' + NETWORKS[network] + '.helloblock.io/v1/'
 
   // end points
   this.addresses = new Addresses(BASE_URL + 'addresses')
