bac520de86a28525780d25c433cfe993c7bea609
Daniel Cousens <github@dcousens.com>
2014-09-22 21:15:16 +1000

    Blocks: add latest impl

--- src/blocks.js
+++ src/blocks.js
@@ -1,4 +1,6 @@
 var assert = require('assert')
+var request = require('superagent')
+var utils = require('./utils')
 
 function Blocks(url) {
   this.url = url
@@ -12,8 +14,33 @@ Blocks.prototype.get = function(params, callback) {
   assert(false, 'TODO')
 }
 
-Blocks.prototype.latest = function(params, callback) {
-  assert(false, 'TODO')
+Blocks.prototype.latest = function(count, callback) {
+  // optional count
+  if ('function' === typeof count) {
+    callback = count
+    count = 1
+  }
+
+  var query = '/latest?=' + ('&limit=' + (count || 1))
+
+  console.log(this.url + query)
+
+  request
+  .get(this.url + query)
+  .end(utils.handleJSend(function(data) {
+    return data.blocks.map(function(block) {
+      return {
+        blockHash: block.blockHash,
+        merkleRootHash: block.merkleRootHash,
+        prevBlockHash: block.prevBlockHash,
+        nonce: block.nonce,
+        blockHeight: block.blockHeight,
+        blockTime: block.blockTime,
+        blockSize: block.bits,
+        txCount: block.txsCount
+      }
+    })
+  }, callback))
 }
 
 Blocks.prototype.propagate = function(params, callback) {
